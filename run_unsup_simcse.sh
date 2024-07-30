73dc94bb293e0ddc8997acdde6acf9cafb2a497d
LyuShawn <lyushawn@foxmail.com>
2024-07-30 23:47:00 +0800

    update:      ，

--- run_unsup_simcse.sh
+++ run_unsup_simcse.sh
@@ -28,23 +28,4 @@
 #     --seed 579 \
 #     "$@"
 
-python train.py \
-    --model_name_or_path bert-base-uncased \
-    --train_file 'data/wiki1m_knowledge.txt' \
-    --output_dir result/29 \
-    --num_train_epochs 1 \
-    --per_device_train_batch_size 64 \
-    --learning_rate 3e-5 \
-    --max_seq_length 64 \
-    --evaluation_strategy steps \
-    --metric_for_best_model stsb_spearman \
-    --load_best_model_at_end \
-    --eval_steps 125 \
-    --pooler_type cls \
-    --mlp_only_train \
-    --overwrite_output_dir \
-    --temp 0.05 \
-    --do_eval \
-    --fp16 \
-    --seed 579 \
-    "$@"
+python train.py --output_dir result/33
