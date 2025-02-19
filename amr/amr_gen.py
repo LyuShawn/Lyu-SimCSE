ff0fa0cb6b7872a20815de0b6d26912e0ca15deb
LyuShawn <lyushawn@foxmail.com>
2025-02-19 10:32:18 +0800

    update

--- amr/amr_gen.py
+++ amr/amr_gen.py
@@ -12,7 +12,7 @@ def main(args):
         sent_list = f.read().splitlines()
 
     print(f"Loading models...")
-    stog = amrlib.load_stog_model("model/model_parse_xfm_bart_base-v0_1_0")
+    stog = amrlib.load_stog_model(args.stog_model)
     stog.batch_size = bs
     gtos = amrlib.load_gtos_model("model/model_generate_t5wtense-v0_1_0")
     gtos.batch_size = bs
@@ -22,13 +22,14 @@ def main(args):
         list = sent_list[i:i+bs]
         graphs = stog.parse_sents(list)
         sents = gtos.generate(graphs)
-        with open(output_file, 'a', encoding='utf-8') as f:
+        with open(output_file, 'w', encoding='utf-8') as f:
             f.write('\n'.join(sents[0]) + '\n')
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--input_file', type=str, default='data/wiki1m_for_simcse.txt')
     parser.add_argument('--output_file', type=str, default='data/wiki1m_for_simcse_amr.txt')
+    parser.add_argument('--stog_model', type=str, default='model/model_parse_xfm_bart_base-v0_1_0')
     parser.add_argument('--bs', type=int, default=16)
     args = parser.parse_args()
     main(args)
