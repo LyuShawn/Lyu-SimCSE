cd5a34c94ff824dd2a63c3d01010bb818292983f
LyuShawn <lyushawn@foxmail.com>
2025-03-19 14:27:34 +0800

    update

--- simcse/models.py
+++ simcse/models.py
@@ -240,6 +240,7 @@ def cl_forward(cls,
         outputs1 = encoder(
             input_ids=input_ids1,
             attention_mask=attention_mask1,
+            output_hidden_states=True if cls.model_args.pooler_type in ['avg_top2', 'avg_first_last'] else False,
             return_dict=True,
         )
         pooler_output1 = cls.pooler(attention_mask1, outputs1, input_ids1, cls.mask_token_id)
@@ -253,6 +254,9 @@ def cl_forward(cls,
         )
         pooler_output2 = cls.pooler(attention_mask2, outputs2, input_ids2, cls.mask_token_id)
         z2 = cls.mlp(pooler_output2)
+
+        assert z1.shape == z2.shape == (batch_size, hidden_dim), "z1 and z2 should have the same shape"
+
         outputs = outputs2
 
     else:
