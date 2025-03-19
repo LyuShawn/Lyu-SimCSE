51b32ba4849d0fca05bfba0ac19638de7a984c53
LyuShawn <lyushawn@foxmail.com>
2025-03-19 19:57:39 +0800

    update

--- loader/loader.py
+++ loader/loader.py
@@ -138,13 +138,6 @@ def prepare_features(examples, args:PrepareFeaturesArgs):
 
         sent_features['input_ids'] = input_ids
         sent_features['attention_mask'] = attention_mask
-        # 对齐
-        # sent_features['attention_mask'] = []
-        # ml = max([len(i) for i in sent_features['input_ids']])
-        # for i in range(len(sent_features['input_ids'])):
-        #     t = sent_features['input_ids'][i]
-        #     sent_features['input_ids'][i] = t + [tokenizer.pad_token_id]*(ml-len(t))
-        #     sent_features['attention_mask'].append(len(t)*[1] + (ml-len(t))*[0])
 
     else:
         sent_features = tokenizer(
